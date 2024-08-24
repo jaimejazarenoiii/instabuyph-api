5a93dd97a86fe2cd1925fc9aee685db53802f4ad
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2024-08-24 21:35:13 +0800

    Add initial setup for router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import express, { Express, Request, Response } from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import router from './routes/routes';
+
+dotenv.config();
 
 const app: Express = express();
+const mongoString: string | undefined = process.env.DATABASE_URL
+mongoose.connect(mongoString ?? '');
+const database = mongoose.connection;
+
+database.on('error', (error: Error) => {
+  console.log(error);
+})
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+app.use(express.json());
+app.use('/api', router);
 
 app.get("/", (req: Request, res: Response) => {
  res.status(200).json("Hello from the server!!!");
